125921c5fd3c38c3886afb874736ac9ed7243c6c
Huu Phong Nguyen <huuphong.nguyen1507@gmail.com>
2021-02-12 16:48:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
 import Container from "../components/Container";
 import ProductsGrid from "../components/elements/ProductsGrid";
 
-const defaultEndpoint = process.env.NOTION_UNOFFICIAL_API;
-
-export async function getServerSideProps() {
-  const res = await fetch(defaultEndpoint);
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default function Home({ data }) {
-  const embarray = [data];
-
+export default function Home() {
   return (
     <Container>
       <div className="flex flex-col mx-auto bg-gray-100 dark:bg-gray-900 items-start max-w-4xl my-2 md:mt-10 p-3 md:p-10 rounded-md w-full">
@@ -24,9 +9,7 @@ export default function Home({ data }) {
           Hi, there!
         </h1>
       </div>
-      <div className="mx-auto bg-gray-100 dark:bg-gray-900 items-start max-w-4xl my-2 md:my-10 p-3 md:p-10 rounded-md w-full">
-        <ProductsGrid data={embarray[0]} />
-      </div>
+      <div className="mx-auto bg-gray-100 dark:bg-gray-900 items-start max-w-4xl my-2 md:my-10 p-3 md:p-10 rounded-md w-full"></div>
     </Container>
   );
 }
